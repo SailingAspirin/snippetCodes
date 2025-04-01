3355534e483881b9f5cfb83d0a306fc3f2249a2d
zh <1194723685@qq.com>
2025-04-01 14:38:27 +0800

    feat(renderer): 优化搜索框支持中文输入
    - 添加 useChineseInput 钩子以支持中文输入法的正确处理
    - 实现搜索框组件的输入状态管理
    - 添加搜索功能的副作用处理

--- src/renderer/src/components/ContentSearch/index.tsx
+++ src/renderer/src/components/ContentSearch/index.tsx
@@ -1,8 +1,20 @@
 import { Add } from '@icon-park/react'
+import useChineseInput from '@renderer/hooks/useChineseInput'
+import { useEffect, useState } from 'react'
 import { Form, useSubmit } from 'react-router-dom'
 
 function ContentSearch() {
   const submit = useSubmit()
+  const [search, setSearch] = useState('')
+  const { inputValue, handleInput, handleCompositionStart, handleCompositionEnd } = useChineseInput(
+    search,
+    setSearch
+  )
+
+  useEffect(() => {
+    submit({ searchWord: search }, { method: 'get' })
+  }, [search])
+
   return (
     <Form>
       <div className="border-b flex justify-between items-center">
@@ -11,7 +23,10 @@ function ContentSearch() {
           type="text"
           placeholder="搜索..."
           className="outline-none text-sm py-2 px-3 w-full"
-          onChange={(e) => submit(e.target.form)}
+          value={inputValue}
+          onInput={handleInput}
+          onCompositionStart={handleCompositionStart}
+          onCompositionEnd={handleCompositionEnd}
         />
         <Add
           theme="outline"
